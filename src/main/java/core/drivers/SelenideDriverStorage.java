640607fb0782ffed8b3aada5b39b617e17cd0698
tod-eug <tod-eug@yandex.ru>
2019-12-31 17:36:20 +0300

    Driver killer implementation

--- src/main/java/core/drivers/SelenideDriverStorage.java
+++ src/main/java/core/drivers/SelenideDriverStorage.java
@@ -7,10 +7,28 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import static java.lang.Thread.currentThread;
 
+/**
+ * Map driverMap contains all webDrivers. You can get driver by running command
+ * <code>SelenideDriverStorage.getDriver()</code>. After end of test driver will be killed
+ * by Driver killer.
+ *
+ * For example your test can be written as follows:
+ *
+ * Given some preconditions
+ * And some other preconditions
+ * When we create new webDriver
+ * And open some web page
+ * And do something
+ * Then something will be...
+ */
 public class SelenideDriverStorage {
 
     private static Map<Long, SelenideDriver> driverMap = new ConcurrentHashMap<>();
 
+    /**
+     * Create new webDriver and start Driver killer for this driver
+     * @return new webDriver
+     */
     public static SelenideDriver getDriver() {
         SelenideDriver driver;
         if (driverMap.containsKey(currentThread().getId())) {
@@ -18,7 +36,16 @@ public class SelenideDriverStorage {
         } else {
             driver = new SelenideDriver(new CustomConfig());
             driverMap.put(currentThread().getId(), driver);
+            new DriverKiller(currentThread().getId()).start();
         }
         return driver;
+    }
+
+    /**
+     * Close webDriver by threadId
+     * @param id threadId
+     */
+    public static void closeDriver(Long id) {
+        driverMap.get(id).close();
     }
 }
